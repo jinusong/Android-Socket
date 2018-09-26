c080585223803bc61a30f7167162603fe3900af6
jinusong <sugwer7464@gmail.com>
2018-09-26 23:41:37 +0900

    [Update] Kotlin Socket application

--- Socket/app/src/main/java/com/jinwoo/socket/MainActivity.kt
+++ Socket/app/src/main/java/com/jinwoo/socket/MainActivity.kt
@@ -11,12 +11,9 @@ import org.json.JSONException
 class MainActivity : AppCompatActivity() {
 
     lateinit var Text : TextView
-    lateinit var Receive_Text : TextView
     lateinit var lighton_btn: Button
     lateinit var lightoff_btn: Button
     lateinit var socket: Socket
-    lateinit var receive_data : String
-    lateinit var receive_btn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,33 +22,23 @@ class MainActivity : AppCompatActivity() {
         lighton_btn = findViewById(R.id.lighton_button)
         lightoff_btn = findViewById(R.id.lightoff_button)
         Text = findViewById(R.id.message)
-        Receive_Text = findViewById(R.id.receive_Text)
-        receive_btn = findViewById(R.id.receive_btn)
 
         socket = SocketApplication.get()
 
         Text.setText("소켓 생성")
 
-        socket.on("lightOn",light_on)
-        socket.on("lightOff", light_off)
         socket.connect()
 
         lighton_btn.setOnClickListener({ v ->
             socket.emit("lightOn")
+            Text.setText("Light on Emit 성공")
         })
 
         lightoff_btn.setOnClickListener({ v ->
             socket.emit("lightOff")
+            Text.setText("Light off Emit 성공")
         })
     }
 
-    var light_on = Emitter.Listener { args ->
-        Text.setText("소켓 on 성공")
-        Receive_Text.setText(args.toString())
-    }
 
-    var light_off = Emitter.Listener { args ->
-        Text.setText("소켓 on 성공")
-        Receive_Text.setText(args.toString())
-    }
 }
